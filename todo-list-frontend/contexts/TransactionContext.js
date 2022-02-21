c40b0cdf4feff86c8db4d3c1a3c104a480aadec8
Abdullah Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2022-02-21 18:14:59 +0300

    prettier setup

--- todo-list-frontend/contexts/TransactionContext.js
+++ todo-list-frontend/contexts/TransactionContext.js
@@ -9,156 +9,155 @@ import { toast } from "react-toastify";
 
 const TransactionContext = createContext();
 
-const TransactionContextProvider = (props) => {
-  const [transactionList, setTransactionList] = useState([]);
-  const [userData, setUserData] = useState({
-    account: "",
-    isContractOwner: false,
-  });
+const TransactionContextProvider = props => {
+	const [transactionList, setTransactionList] = useState([]);
+	const [userData, setUserData] = useState({
+		account: "",
+		isContractOwner: false,
+	});
 
-  const [web3Instance, setWeb3Instance] = useState(null);
-  const [smartContractInstance, setSmartContractInstance] = useState(null);
-  const { user, authenticate, logout } = useMoralis();
+	const [web3Instance, setWeb3Instance] = useState(null);
+	const [smartContractInstance, setSmartContractInstance] = useState(null);
+	const { user, authenticate, logout } = useMoralis();
 
-  useEffect(() => {
-    async function load() {
-      const web3 = await getWeb3Instance();
-      const contactList = await getContractInstance(web3);
+	useEffect(() => {
+		async function load() {
+			const web3 = await getWeb3Instance();
+			const contactList = await getContractInstance(web3);
 
-      setWeb3Instance(web3);
-      setSmartContractInstance(contactList);
+			setWeb3Instance(web3);
+			setSmartContractInstance(contactList);
 
-      const networkId = await web3.eth.net.getId();
-      const networkname = getNetworkName(networkId);
+			const networkId = await web3.eth.net.getId();
+			const networkname = getNetworkName(networkId);
 
-      const result = checkUserInCorrectChain(CHAIN_TYPES.GANACHE_TESTNET);
-      console.log({ result });
-      console.log({ networkId });
-      console.log({ networkname });
+			const result = checkUserInCorrectChain(CHAIN_TYPES.GANACHE_TESTNET);
+			console.log({ result });
+			console.log({ networkId });
+			console.log({ networkname });
 
-      if (!result) {
-        toast.error(`
+			if (!result) {
+				toast.error(`
           You are in: ${networkname}. You need to connect Ganache Local Network.
         `);
-      }
-    }
+			}
+		}
 
-    load();
-  }, []);
+		load();
+	}, []);
 
-  function getNetworkName(chainID) {
-    const networks = {
-      1: "Ethereum Mainnet",
-      4: "Ethereum Rinkeby",
-      42: "Kovan Testnet",
-      97: "Binance Smart Chain Testnet",
-      5777: "Ganache Local Network",
-      80001: "Polygon Mumbai Testnet",
-    };
-    return networks[chainID];
-  }
+	function getNetworkName(chainID) {
+		const networks = {
+			1: "Ethereum Mainnet",
+			4: "Ethereum Rinkeby",
+			42: "Kovan Testnet",
+			97: "Binance Smart Chain Testnet",
+			5777: "Ganache Local Network",
+			80001: "Polygon Mumbai Testnet",
+		};
+		return networks[chainID];
+	}
 
-  useEffect(() => {
-    const initUser = async () => {
-      const account = user.get("ethAddress");
-      const isContractOwner = await hasUserAccountOwner(account);
+	useEffect(() => {
+		const initUser = async () => {
+			const account = user.get("ethAddress");
+			const isContractOwner = await hasUserAccountOwner(account);
 
-      setUserData({
-        account,
-        isContractOwner,
-      });
-    };
-    if (user && smartContractInstance) {
-      initUser();
-    } else {
-      setUserData({
-        account: "",
-        isContractOwner: false,
-      });
-    }
-  }, [user, smartContractInstance]);
+			setUserData({
+				account,
+				isContractOwner,
+			});
+		};
+		if (user && smartContractInstance) {
+			initUser();
+		} else {
+			setUserData({
+				account: "",
+				isContractOwner: false,
+			});
+		}
+	}, [user, smartContractInstance]);
 
-  const getWeb3Instance = async () => {
-    const localGanacheRPCUrl = new Web3.providers.HttpProvider(
-      "http://localhost:7545"
-    );
+	const getWeb3Instance = async () => {
+		const localGanacheRPCUrl = new Web3.providers.HttpProvider(
+			"http://localhost:7545"
+		);
 
-    // return new Web3(localGanacheRPCUrl);
-    return new Web3(Web3.givenProvider || localGanacheRPCUrl);
-  };
+		return new Web3(Web3.givenProvider || localGanacheRPCUrl);
+	};
 
-  // Returns contactSmartContractInstance
-  const getContractInstance = async (web3) => {
-    // Instantiate smart contract using ABI and address.
-    return new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-  };
+	// Returns contactSmartContractInstance
+	const getContractInstance = async web3 => {
+		// Instantiate smart contract using ABI and address.
+		return new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+	};
 
-  const getStoredBalance = async () => {
-    if (!userData.account) {
-      return "0";
-    }
+	const getStoredBalance = async () => {
+		if (!userData.account) {
+			return "0";
+		}
 
-    return (await smartContractInstance.methods.getStoredBalance()).call({
-      from: userData.account,
-    });
-  };
+		return (await smartContractInstance.methods.getStoredBalance()).call({
+			from: userData.account,
+		});
+	};
 
-  const getStoredBalanceOfUser = async (account) => {
-    if (!userData.account) {
-      return Number.NaN;
-    }
+	const getStoredBalanceOfUser = async account => {
+		if (!userData.account) {
+			return Number.NaN;
+		}
 
-    if (!userData.isContractOwner) {
-      throw new Error("Not a contract owner");
-    }
+		if (!userData.isContractOwner) {
+			throw new Error("Not a contract owner");
+		}
 
-    return (await smartContractInstance.methods.getStoredBalance(account)).call(
-      { from: userData.account }
-    );
-  };
+		return (await smartContractInstance.methods.getStoredBalance(account)).call(
+			{ from: userData.account }
+		);
+	};
 
-  const getOwner = async () => {
-    return await smartContractInstance.methods.owner().call();
-  };
+	const getOwner = async () => {
+		return await smartContractInstance.methods.owner().call();
+	};
 
-  const connectWallet = async () => {
-    // For some reason user returns as undefined
-    await authenticate({ signingMessage: "Connect Account with Moralis" });
-  };
+	const connectWallet = async () => {
+		// For some reason user returns as undefined
+		await authenticate({ signingMessage: "Connect Account with Moralis" });
+	};
 
-  const hasUserAccountOwner = async (account) => {
-    return (
-      await smartContractInstance.methods.hasUserAccountOwner(account)
-    ).call();
-  };
+	const hasUserAccountOwner = async account => {
+		return (
+			await smartContractInstance.methods.hasUserAccountOwner(account)
+		).call();
+	};
 
-  const logoutUser = async () => {
-    // For some reason user returns as undefined
-    await logout();
-  };
+	const logoutUser = async () => {
+		// For some reason user returns as undefined
+		await logout();
+	};
 
-  console.log({ userData });
+	console.log({ userData });
 
-  return (
-    <TransactionContext.Provider
-      value={{
-        transactionList,
-        web3Instance,
-        smartContractInstance,
-        account: userData.account,
-        userData,
-        getOwner,
-        getContractInstance,
-        setTransactionList,
-        connectWallet,
-        logoutUser,
-        getStoredBalance,
-        getStoredBalanceOfUser,
-      }}
-    >
-      {props.children}
-    </TransactionContext.Provider>
-  );
+	return (
+		<TransactionContext.Provider
+			value={{
+				transactionList,
+				web3Instance,
+				smartContractInstance,
+				account: userData.account,
+				userData,
+				getOwner,
+				getContractInstance,
+				setTransactionList,
+				connectWallet,
+				logoutUser,
+				getStoredBalance,
+				getStoredBalanceOfUser,
+			}}
+		>
+			{props.children}
+		</TransactionContext.Provider>
+	);
 };
 
 export { TransactionContextProvider, TransactionContext };
--- todo-list-frontend/pages/todos/index.js
+++ todo-list-frontend/pages/todos/index.js
@@ -2,154 +2,165 @@ import { hello } from "config";
 import { TransactionContext } from "contexts/TransactionContext";
 import useTransactionListener from "hooks/useTransactionListener";
 import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { TRANSACTION_NAMES } from "utils/enums";
 
 const TodosPage = () => {
-  const {
-    userData,
-    connectWallet,
-    logoutUser,
-    smartContractInstance,
-    transactionList,
-    setTransactionList,
-  } = useContext(TransactionContext);
+	const {
+		userData,
+		connectWallet,
+		logoutUser,
+		smartContractInstance,
+		transactionList,
+		setTransactionList,
+	} = useContext(TransactionContext);
 
-  const [todoList, setTodoList] = useState([]);
-  const [newTodoText, setNewTodoText] = useState("");
+	const [todoList, setTodoList] = useState([]);
+	const [newTodoText, setNewTodoText] = useState("");
 
-  const { lastFinishedTransaction } = useTransactionListener();
+	const { lastFinishedTransaction } = useTransactionListener();
 
-  useEffect(() => {
-    if (smartContractInstance) {
-      getTodos();
-    }
-  }, [smartContractInstance]);
+	useEffect(() => {
+		if (smartContractInstance) {
+			getTodos();
+		}
+	}, [smartContractInstance]);
 
-  useEffect(() => {
-    if (lastFinishedTransaction) {
-      getTodos();
-    }
-  }, [lastFinishedTransaction]);
+	useEffect(() => {
+		if (lastFinishedTransaction) {
+			getTodos();
+		}
+	}, [lastFinishedTransaction]);
 
-  const getTodos = async () => {
-    const todoCount = await smartContractInstance.methods.todoCount().call();
+	const getTodos = async () => {
+		const todoCount = await smartContractInstance.methods.todoCount().call();
 
-    const totalList = [];
-    for (let i = 1; i <= todoCount; i++) {
-      const { id, content, completed, isExist } =
-        await smartContractInstance.methods.getTodo(i).call();
+		const totalList = [];
+		for (let i = 1; i <= todoCount; i++) {
+			const { id, content, completed, isExist } =
+				await smartContractInstance.methods.getTodo(i).call();
 
-      totalList.push({
-        id,
-        content,
-        completed,
-        isExist,
-      });
-    }
+			totalList.push({
+				id,
+				content,
+				completed,
+				isExist,
+			});
+		}
 
-    setTodoList(totalList);
-  };
+		setTodoList(totalList);
+	};
 
-  const handleAddNewTodo = async (e) => {
-    e.preventDefault();
+	const handleAddNewTodo = async e => {
+		e.preventDefault();
 
-    const tx = await smartContractInstance.methods
-      .createTodo(newTodoText)
-      .send({ from: userData.account });
-    console.log({ tx });
+		if (!userData?.account) {
+			toast.error("User is not connected");
+			throw new Error("user is not connected ");
+		}
 
-    setTransactionList([
-      ...transactionList,
-      { ...tx, hash: tx.transactionHash, name: TRANSACTION_NAMES.ADD_TODO },
-    ]);
-  };
+		const tx = await smartContractInstance.methods
+			.createTodo(newTodoText)
+			.send({ from: userData.account });
+		console.log({ tx });
 
-  const handleToggleTodo = async (todo) => {
-    try {
-      const tx = await smartContractInstance.methods
-        .toggleCompleted(todo.id)
-        .send({ from: userData?.account });
+		setTransactionList([
+			...transactionList,
+			{ ...tx, hash: tx.transactionHash, name: TRANSACTION_NAMES.ADD_TODO },
+		]);
+	};
 
-      console.log({ tx });
+	const handleToggleTodo = async todo => {
+		try {
+			if (!userData?.account) {
+				toast.error("User is not connected");
+				throw new Error("user is not connected ");
+			}
 
-      setTodoList(
-        todoList.map((_todo) => {
-          if (_todo.id == todo.id) {
-            return {
-              ..._todo,
-              completed: !_todo.completed,
-            };
-          }
-          return _todo;
-        })
-      );
-    } catch (err) {
-      console.log({ err });
-    }
-  };
+			const tx = await smartContractInstance.methods
+				.toggleCompleted(todo.id)
+				.send({ from: userData?.account });
 
-  console.log({ lastFinishedTransaction });
-  return (
-    <div className="todos">
-      <div className="app-container">
-        <div className="account">
-          {userData?.account ? (
-            <div className="connected">
-              <div className="text">{userData?.account}</div>
-              <button className="logout-btn" onClick={logoutUser}>
-                Logout
-              </button>
-            </div>
-          ) : (
-            <button className="connect-btn" onClick={connectWallet}>
-              Connect Wallet
-            </button>
-          )}
-        </div>
-      </div>
+			console.log({ tx });
 
-      <div className="add-todo-container">
-        <form onSubmit={handleAddNewTodo}>
-          <div>
-            <input
-              type="text"
-              name="add-todo"
-              id="add-todo"
-              value={newTodoText}
-              onChange={(e) => {
-                setNewTodoText(e.target.value);
-              }}
-            />
-            <input type="submit" value="Create New Todo" />
-          </div>
-        </form>
-      </div>
+			setTodoList(
+				todoList.map(_todo => {
+					if (_todo.id == todo.id) {
+						return {
+							..._todo,
+							completed: !_todo.completed,
+						};
+					}
+					return _todo;
+				})
+			);
+		} catch (err) {
+			console.log({ err });
+		}
+	};
 
-      <div className="todos-container">
-        <h4>Todo List</h4>
-        <ul>
-          {todoList.length > 0
-            ? todoList.map((todo) => (
-                <li key={todo.id}>
-                  <label htmlFor={todo.id}>
-                    <input
-                      type="checkbox"
-                      name={todo.id}
-                      id={todo.id}
-                      checked={todo.completed}
-                      onChange={(e) => {
-                        handleToggleTodo(todo);
-                      }}
-                    />
-                    {todo.content}
-                  </label>
-                </li>
-              ))
-            : null}
-        </ul>
-      </div>
-    </div>
-  );
+	console.log({ lastFinishedTransaction });
+	return (
+		<div className="todos">
+			<div className="app-container">
+				<div className="account">
+					{userData?.account ? (
+						<div className="connected">
+							<div className="text">{userData?.account}</div>
+							<button className="logout-btn" onClick={logoutUser}>
+								Logout
+							</button>
+						</div>
+					) : (
+						<button className="connect-btn" onClick={connectWallet}>
+							Connect Wallet
+						</button>
+					)}
+				</div>
+			</div>
+
+			<div className="add-todo-container">
+				<form onSubmit={handleAddNewTodo}>
+					<div>
+						<input
+							type="text"
+							name="add-todo"
+							id="add-todo"
+							value={newTodoText}
+							onChange={e => {
+								setNewTodoText(e.target.value);
+							}}
+						/>
+						<input type="submit" value="Create New Todo" />
+					</div>
+				</form>
+			</div>
+
+			<div className="todos-container">
+				<h4>Todo List</h4>
+				<ul>
+					{todoList.length > 0
+						? todoList.map(todo => (
+								<li key={todo.id}>
+									<label htmlFor={todo.id}>
+										<input
+											type="checkbox"
+											name={todo.id}
+											id={todo.id}
+											checked={todo.completed}
+											onChange={e => {
+												handleToggleTodo(todo);
+											}}
+										/>
+										{todo.content}
+									</label>
+								</li>
+						  ))
+						: null}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default TodosPage;
